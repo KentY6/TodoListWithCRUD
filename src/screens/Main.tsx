2ee412b0d672efb37624e0884c7e46e5d4e06fb7
KentY <e6ony.ivory@gmail.com>
2023-07-31 20:53:32 +0900

    Firebase設定

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Title } from "../components/atoms/Title";
 import { TodoInputForm } from "../components/molecules/TodoInputForm";
 import { TodoList } from "../components/molecules/TodoList";
+import { Link } from "react-router-dom";
 
 // Todoの型
 export interface Todo {
@@ -33,6 +34,10 @@ export const Main = () => {
       <div className="flex flex-col items-center justify-center">
         <Title title={"TodoList"} />
         <TodoInputForm addTodo={addTodo} />
+        <div className="text-blue-500 m-5 border-b border-blue-500">
+          <Link to={`/login/`}>ログインフォームへ</Link>
+        </div>
+
         <TodoList todos={todos} deleteTodo={deleteTodo} />
       </div>
     </>
