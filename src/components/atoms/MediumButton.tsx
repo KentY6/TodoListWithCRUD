2005e0e0cf121471bff7cbd97b83c5269071bd2c
KentY <e6ony.ivory@gmail.com>
2023-07-31 21:25:52 +0900

    DBへのTodo保存機能実装

--- src/components/atoms/MediumButton.tsx
+++ src/components/atoms/MediumButton.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
-export const MediumButton = () => {
-  return <>MediumButton</>;
+export const MediumButton = ({ name }: { name: string }) => {
+  return (
+    <>
+      <div className="m-2 bg-red-400 rounded cursor-pointer pl-6 pr-6 p-2">
+        {name}
+      </div>
+    </>
+  );
 };
--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -3,6 +3,10 @@ import { Title } from "../components/atoms/Title";
 import { TodoInputForm } from "../components/molecules/TodoInputForm";
 import { TodoList } from "../components/molecules/TodoList";
 import { Link } from "react-router-dom";
+import { db, auth } from "../firebase";
+import type { User } from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { MediumButton } from "../components/atoms/MediumButton";
 
 // Todoの型
 export interface Todo {
@@ -12,6 +16,7 @@ export interface Todo {
 
 export const Main = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [message, setMessage] = useState<string>("");
 
   // Todo追加機能
   const addTodo: Function = (text: string) => {
@@ -29,11 +34,34 @@ export const Main = () => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  const user: User | null = auth.currentUser;
+
+  // Todoを保存する機能
+  const saveTodosData: Function = async () => {
+    if (user) {
+      const todosDocRef = doc(db, `users/${user.uid}`);
+      try {
+        setMessage("保存中");
+        await setDoc(todosDocRef, {
+          todos: todos,
+        });
+        setMessage("保存に成功しました");
+      } catch (error) {
+        console.error(error);
+        setMessage("保存に失敗しました");
+      }
+    }
+  };
+
   return (
     <>
       <div className="flex flex-col items-center justify-center">
         <Title title={"TodoList"} />
         <TodoInputForm addTodo={addTodo} />
+        <div onClick={() => saveTodosData()}>
+          <MediumButton name={"SAVE"} />
+        </div>
+        <div className="m-2">{message}</div>
         <div className="text-blue-500 m-5 border-b border-blue-500">
           <Link to={`/login/`}>ログインフォームへ</Link>
         </div>
